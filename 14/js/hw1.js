9e010e50b61245db733effc32bd1ee1c73b9a28d
gmlwo25 <wkdgmlwo25@naver.com>
2024-07-02 14:05:02 +0900

    df

---
+++ 14/js/hw1.js
@@ -0,0 +1,9 @@
+var espresso = 10;
+var milk = 170;
+var choco = 50;
+var cream = 60;
+
+console.log(espresso);
+console.log(espresso + milk);
+console.log(espresso + choco + milk);
+console.log(espresso + choco + milk + cream);
